d035655cf3fac537652e3023726b8d9a239aeff0
HenriqueGouveia1 <henriquegs002@gmail.com>
2021-04-21 16:22:30 -0300

    Iniciando o projeto

---
+++ miniprojectheloo/src/components/teste.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Teste extends Component {
+    render() {
+        return (
+            <div>macaco</div>
+        );
+    }
+}
+
+export default Teste
---
+++ miniprojectheloo/src/pages/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+
+
+function App() {
+  return (
+    <div className="App ">
+      macaco
+    </div>
+  );
+}
+
+export default App;
